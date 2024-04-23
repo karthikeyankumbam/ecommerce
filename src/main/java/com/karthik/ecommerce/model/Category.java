55b9025a0732e5ef31cc4954413e1580f5486797
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-04-23 19:51:54 +0530

    Created model classes

---
+++ src/main/java/com/karthik/ecommerce/model/Category.java
@@ -0,0 +1,6 @@
+package com.karthik.ecommerce.model;
+
+public class Category {
+    private long id;
+    private String name;
+}
---
+++ src/main/java/com/karthik/ecommerce/model/Product.java
@@ -0,0 +1,10 @@
+package com.karthik.ecommerce.model;
+
+public class Product {
+    private long id;
+    private String title;
+    private double price;
+    private String description;
+    private String image;
+    private Category category;
+}
